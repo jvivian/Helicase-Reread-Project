9054c4064882fb47c6af2057d270553322985ec1
jvivian <jtvivian@gmail.com>
2014-09-22 13:06:51 -0700

    Creates heatmaps for both context and label regions

--- Scripts/State Heatmap.py
+++ Scripts/State Heatmap.py
@@ -33,12 +33,13 @@ for item in total:
     total_diff.append( abs( np.max(item) - np.min(item) ) )
 
 ## Assemble Final DataFrame
-df = pd.DataFrame( [ C_mC[:16], mC_hmC[:16], C_hmC[:16], total_diff[:16] ] )
+#df = pd.DataFrame( [ C_mC[:16], mC_hmC[:16], C_hmC[:16], total_diff[:16] ] )   # Context
+df = pd.DataFrame( [ C_mC[10:], mC_hmC[10:], C_hmC[10:], total_diff[10:] ] )    # Label
 
 ## Plotting
 # setup
 fig, ax = plt.subplots()
-plt.pcolor(df, cmap='Blues')
+plt.pcolor(df, cmap='Reds')
 
 # Put the major ticks in the middle of each cell
 ax.set_xticks(np.arange(df.shape[1]) +.5, minor=False)
@@ -47,16 +48,18 @@ ax.invert_yaxis()
 plt.xticks(rotation=50)
 
 # Labels
-row_labels = ['C_mC', 'mC_hmC', 'C_hmC', 'Total']
-column_labels = data.columns[1:17]
+row_labels = ['C_mC', 'mC_hmC', 'C_hmC', 'Max $\Delta$ ']
+#column_labels = data.columns[1:17]     # Context
+column_labels = data.columns[11:]       # Label
 column_labels = [x.replace('.1','').replace('.2','') for x in column_labels]
 ax.set_xticklabels(column_labels, minor=False)
 ax.set_yticklabels(row_labels, minor=False)
-plt.gca().set_xlim(0, 16)
+#plt.gca().set_xlim(0, 16)      # Context
+plt.gca().set_xlim(0, 22)       # Label
 plt.title( 'State Importance' )
 
 cb = plt.colorbar()
-cb.set_label('Mean Difference', labelpad=20, fontsize='16')
+cb.set_label('Absolute Mean Difference', labelpad=20, fontsize='16')
 plt.show()
 
 '''
