76958952be1a9aef98b68e933d493493f9cb7a69
John Vivian <jtvivian@gmail.com>
2015-02-23 23:51:16 -0800

    Added rolling average + attempted error bars

--- Scripts/SR-MR-Density-Plot.py
+++ Scripts/SR-MR-Density-Plot.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python2.7
+# John Vivian
+# 1-25-15
 
+'''
 # Define each event as a function x = chunk cutoff [.999, 0]. 
 #
 #  Event  = [.99, .90, .44 ]
@@ -7,7 +11,6 @@
 #  sr = [ (.99, 1), (.98, 1 ) ... (.91, 1) ]   # If Correct 
 #  mr = [ (.90, 0), (.89, 0), ... (.44, 1) ... ] # If first addition incorrect, section correct
 
-'''
 Pseudo-code
 
 SR_FUNCTIONS = [] # Will store our our list of functions
@@ -40,7 +43,6 @@ for event in EVENTS:
 
 ... Alternatively
 
-
 For each POINT average the correct number of SR and MR....  Exception statement for 0s. 
 '''
 
@@ -51,15 +53,21 @@ import Methods
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def return_hardcall(C,L,cutoff,barcode):
+def return_hardcall(C,L,cutoff,barcode,Ind=False):
 
     bchunk, bcall = Methods.best_chunk( C, L )
     ichunk, icall = Methods.ind_consensus( C, L, cutoff )
-
-    if barcode == icall:
-        return 1
+    
+    if Ind:
+        if barcode == icall:
+            return 1
+        else:
+            return 0
     else:
-        return 0
+        if barcode == bcall:
+            return 1
+        else:
+            return 0
 
 
 # Retrieve Events
@@ -67,7 +75,8 @@ with open('..\Data\Ranked_Events\Events.txt', 'r') as f:
     Events = f.readlines()
 
 SR_Master = [] # Will hold 1000 averaged points for single reads
-MR_Master = [] # Will hold 1000 averaged points for multi reads 
+MR_Master_Ind = []
+MR_Master_Best = []
 
 for cutoff in xrange(999,-1,-1):
     cutoff *= .001
@@ -76,16 +85,17 @@ for cutoff in xrange(999,-1,-1):
     sys.stdout.flush()
 
     sr_average = [] # Vectors that will hold values at this cutoff
-    mr_average = [] 
+    mr_average_Ind = []
+    mr_average_Best = []
 
     for event in Events:
         C = event.split('@')[1] # Unpack Contexts 
         C = ast.literal_eval(C) # Cast string as a list
 
-        L = event.split("@")[2]
+        L = event.split("@")[2] 
         L = ast.literal_eval(L)
 
-        barcode = event.split('@')[0].split('-')[0]
+        barcode = event.split('@')[0].split('-')[0] # Correct Call for Event
         
 
         C = [x for x in C if x[0] >= cutoff] # Filter chunks by cutoff
@@ -94,31 +104,76 @@ for cutoff in xrange(999,-1,-1):
             sr_average.append( return_hardcall(C,L,cutoff,barcode) )
 
         elif len(C) > 1:
-            mr_average.append( return_hardcall(C,L,cutoff,barcode) )
+            mr_average_Ind.append( return_hardcall(C,L,cutoff,barcode,True) )
+            mr_average_Best.append( return_hardcall(C,L,cutoff,barcode) )
 
     if sr_average:
         SR_Master.append( np.mean( sr_average) )
     else:
-        SR_Master.append(0)
+        SR_Master.append(np.nan)
 
-    if mr_average:
-        MR_Master.append( np.mean( mr_average) )
+
+    if mr_average_Ind:
+        MR_Master_Ind.append( np.mean( mr_average_Ind) )
     else:
-        MR_Master.append(0)
+        MR_Master_Ind.append( np.nan )
+    
+    if mr_average_Best: 
+       MR_Master_Best.append( np.mean( mr_average_Best) )
+    else:
+        MR_Master_Best.append( np.nan )
 
 
+def rolling_average(X):
+    'computes rolling average on a list'
+    new_X = []
+    std = []
+    window = 15
+    start = -window/2
+    end = window/2
+    for i in xrange(len(X)):
+        if start < 0:
+            new_X.append( np.mean( [x for x in X[0:end] if x!=0 ] ) )
+            std.append( np.std( [x for x in X[0:end] if x!=0 ] ) )
+            start, end = start+1, end+1
+        else:
+            new_X.append( np.mean( [x for x in X[start:end] if x!=0] ) )
+            std.append( np.std( [x for x in X[start:end] if x!=0 ] ) )
+            start, end = start+1, end+1  
+    
+    return new_X, std
+    
 #################
 #   Plotting    #
 #################
 
 X = [i*.001 for i in range(999,-1,-1) ]
 
-plt.plot(X, SR_Master, label='Single Reads', lw=2 )
-plt.plot(X, MR_Master, label='Multiple Reads', lw=2)
+SR_Master, SR_std = rolling_average(SR_Master)
+MR_Master_Best, MR_std = rolling_average(MR_Master_Best)
+#MR_Master_Ind = rolling_average(MR_Master_Ind)
 
+SR_plus = []
+SR_minus = []
+for i in xrange(len(SR_Master)):
+    SR_plus.append( SR_Master[i]+SR_std[i] )
+    SR_minus.append( SR_Master[i]-SR_std[i] )
+
+MR_plus = []
+MR_minus = []
+for i in xrange(len(MR_Master_Best)):
+    MR_plus.append( MR_Master_Best[i]+MR_std[i] )
+    MR_minus.append( MR_Master_Best[i]-MR_std[i] )
+    
+
+plt.plot(X, SR_Master, label='Single Reads', lw=2 )
+#plt.plot(X, MR_Master_Ind, label='Independent Consensus', lw=2)
+plt.plot(X, MR_Master_Best, label='Best Consensus', lw=2)
 ax = plt.gca()
 ax.invert_xaxis() 
-ax.set_ylim([0.5,1])
+ax.set_ylim([0.6,0.85])
+#ax.fill_between(X, SR_plus, SR_minus, alpha=0.5 )
+#ax.fill_between(X, MR_plus, MR_minus, alpha=0.5)
 
 plt.title('Density of Single vs. Multiple Reads', fontsize=20)
 plt.xlabel('Chunk Cutoff', fontsize=14)
